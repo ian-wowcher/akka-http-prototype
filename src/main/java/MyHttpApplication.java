90c52fa7a5e3cc02d2944cfbe0e562fa16861d95
Ian Polding <ipolding@gmail.com>
2015-05-12 00:06:29 +0100

    Added http

---
+++ src/main/java/MyHttpApplication.java
@@ -0,0 +1,19 @@
+import akka.actor.ActorSystem;
+import akka.http.javadsl.server.Directives;
+import akka.http.javadsl.server.HttpApp;
+import akka.http.javadsl.server.HttpService;
+import akka.http.javadsl.server.Route;
+
+public class MyHttpApplication extends HttpApp{
+
+    /* TODO Implement the HttpApp.createRoute method to provide the Route
+    * and then call HttpApp.bindRoute to start the server on the specified interface.
+    * */
+    @Override
+    public Route createRoute() {
+
+
+
+        return null;
+    }
+}
